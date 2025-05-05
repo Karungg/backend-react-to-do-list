502f371096c89bc696937f3dec7f45cfbc490e03
Karungg <miftahfadilah71@gmail.com>
2025-05-05 10:47:27 +0700

    Initial commit

---
+++ src/main/java/com/todolist/react_to_do_list/ReactToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.todolist.react_to_do_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactToDoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todolist/react_to_do_list/ReactToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist.react_to_do_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
