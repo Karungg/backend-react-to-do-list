c98e18de8c1e895fe951c2a515b8780aeb34434f
Karungg <miftahfadilah71@gmail.com>
2025-05-06 05:21:59 +0700

    Modify task model

--- src/main/java/com/todolist/react_to_do_list/models/Task.java
+++ src/main/java/com/todolist/react_to_do_list/models/Task.java
@@ -10,12 +10,16 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "tasks")
 public class Task {
 
     @Id
@@ -28,4 +32,8 @@ public class Task {
 
     @Enumerated(EnumType.STRING)
     private Status status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
