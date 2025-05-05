1ff66657b03ad3d887274ebb3a0d70b2bb34ed72
Karungg <miftahfadilah71@gmail.com>
2025-05-06 05:22:17 +0700

    Modify user model

--- src/main/java/com/todolist/react_to_do_list/models/User.java
+++ src/main/java/com/todolist/react_to_do_list/models/User.java
@@ -1,26 +1,44 @@
 package com.todolist.react_to_do_list.models;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
+    @NotNull
     private String name;
 
+    @Email
+    @Column(unique = true)
     private String email;
 
+    @JsonIgnore
     private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Task> task = new ArrayList<>();
 }
